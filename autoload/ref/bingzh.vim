bootleq/vim-ref-bingzh

commit 5474abcd0b293f7ec7c0294cbd302b6a28afc98a

Fix quote-escape problem during opencc transform.

autoload/ref/bingzh.vim
@@ -54,7 +54,8 @@ function! s:source.get_body(query) "{{{
 
   " 簡體 => 繁體
   if executable('opencc')
-    let result = system(printf("echo '%s' | opencc", result))
+    let cmd = printf("echo -n '%s' | opencc", substitute(result, "'", "''", 'g'))
+    let result = system(cmd)
   endif
 
   let result = s:after_formatter(result)

commit 440468920a833013aaafa1b0c810e763f59c5332

Fix the stress mark disappear in pronounce
Seems broken since 5474abcd.

autoload/ref/bingzh.vim
@@ -24,7 +24,7 @@ function! s:source.get_body(query) "{{{
 
   " 輸出時，簡體轉為繁體
   if executable('opencc')
-    let cmd = printf("echo -n '%s' | opencc", substitute(result, "'", "''", 'g'))
+    let cmd = printf("echo -En '%s' | opencc", substitute(result, "'", '''"''"''', 'g'))
     let result = system(cmd)
   endif
 